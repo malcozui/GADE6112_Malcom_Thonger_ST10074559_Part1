6f5e272324b5183d45a34300ce6c5175eb39d532
Malcom Thonger <malcozui@gmail.com>
2022-07-25 15:51:47 +0200

    finished SwampCreature

--- SwampCreature.cs
+++ SwampCreature.cs
@@ -19,29 +19,30 @@ namespace GADE6112_Malcom_Thonger_ST10074559_Part1
         {
             Random rndm = new Random();
             bool loop = false;
+            int dir;
             do
             {
-                int dir = rndm.Next(4);
-                switch (dir)
+                dir = rndm.Next(4);
+                if (cardinalTiles[dir] != TileType.EmptyTile)
                 {
-                    case 0:
-                        if (cardinalTiles[0] != TileType.EmptyTile)
-                        {
-
-                        }
-                        return Movement.Up;
-                    case 1:
-                        return Movement.Down;
-                    case 2:
-                        return Movement.Left;
-                    case 3:
-                        return Movement.Right;
-                    default:
-                        //Will never be called but the compiler throws an
-                        //"not all code paths return a value" error if i dont add this
-                        return Movement.NoMovement;
+                    loop = true;
                 }
             } while (loop);
+            switch (dir)
+            {
+                case 0:
+                    return Movement.Up;
+                case 1:
+                    return Movement.Down;
+                case 2:
+                    return Movement.Left;
+                case 3:
+                    return Movement.Right;
+                default:
+                    //Will never be called but the compiler throws an
+                    //"not all code paths return a value" error if i dont add this
+                    return Movement.NoMovement;
+            }
         }
     }
 }
