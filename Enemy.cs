f18bd60c1fe33d88a6897ebf04874c69a5b78a1b
Malcom Thonger <malcozui@gmail.com>
2022-07-25 13:58:50 +0200

    enemy abstract class

---
+++ Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_Malcom_Thonger_ST10074559_Part1
+{
+    abstract class Enemy : Character
+    {
+        protected object randomiser;
+        protected Enemy(int x, int y, TileType tileType, int hp, int maxHp, int dmg) 
+            :base(x, y, tileType, hp, maxHp, dmg)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Enemy at [{x},{y}] ({dmg})";
+        }
+    }
+}
