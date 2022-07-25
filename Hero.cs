c319c1d2e6633b37e1ca3ea949e4534c62491c55
Malcom Thonger <malcozui@gmail.com>
2022-07-25 15:54:25 +0200

    hero class

---
+++ Hero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6112_Malcom_Thonger_ST10074559_Part1
+{
+    internal class Hero : Character
+    {
+        public Hero(int x, int y, int hp, int maxHp, int dmg) 
+            :base(x, y, hp, maxHp, dmg)
+        {
+
+        }
+
+        public override Movement ReturnMove(Movement move = Movement.NoMovement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
