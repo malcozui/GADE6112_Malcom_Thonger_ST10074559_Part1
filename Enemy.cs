5d37622e4796886c24c3a4a390f689db3c488d03
Malcom Thonger <malcozui@gmail.com>
2022-07-25 15:30:56 +0200

    added symbol to enemy

--- Enemy.cs
+++ Enemy.cs
@@ -9,10 +9,11 @@ namespace GADE6112_Malcom_Thonger_ST10074559_Part1
     abstract class Enemy : Character
     {
         protected object randomiser;
-        protected Enemy(int x, int y, int hp, int maxHp, int dmg) 
-            :base(x, y, hp, maxHp, dmg)
+        protected char symbol;
+        protected Enemy(int x, int y, int hp, int maxHp, int dmg, char symbol)
+            : base(x, y, hp, maxHp, dmg)
         {
-
+            this.symbol = symbol;
         }
         public override string ToString()
         {
