a04c6f88984fb9bcc9b399d46cdf8211289287c8
Malcom Thonger <malcozui@gmail.com>
2022-07-25 13:08:30 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GADE6112_Malcom_Thonger_ST10074559_Part1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
